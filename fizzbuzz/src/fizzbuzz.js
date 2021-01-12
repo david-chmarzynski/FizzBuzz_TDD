d7779c9c3973b6160100891e2ff112f068eb06b9
David Chmarzynski <david.chm00@gmail.com>
2021-01-12 19:06:34 +0100

    FizzBuzz Array OK

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,11 +1,17 @@
-const fizzBuzz = (number) => {
-  if(number % 3 === 0) {
-    return "fizz";
-  }
-  if(number === 5) {
-    return "buzz";
-  }
-  return number;
+const fizzBuzz = (array) => {
+  let result = [];
+  array.map((num) => {
+    if(num % 15 === 0) {
+      return result.push("fizzbuzz");
+    } else if(num % 3 === 0) {
+      return result.push("fizz");
+    } else if(num % 5 === 0) {
+      return result.push("buzz");
+    } else {
+      return result.push(num);
+    }
+  })
+  return result;
 };
 
 export default fizzBuzz;
--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -2,27 +2,47 @@ import fizzBuzz from './fizzbuzz';
 
 describe('Test FizzBuzz', () => {
   test('Should return 1 if 1 given param', () => {
-    const number = 1;
-    expect(fizzBuzz(number)).toBe(number);
+    const number = [1];
+    expect(fizzBuzz(number)).toEqual([1]);
   });
 
   test('Should return 2 if 2 given param', () => {
-    const number = 2;
-    expect(fizzBuzz(number)).toBe(number);
+    const number = [2];
+    expect(fizzBuzz(number)).toEqual([2]);
   });
 
   test('Should return fizz if 3 given param', () => {
-    const number = 3;
-    expect(fizzBuzz(number)).toBe("fizz");
+    const number = [3];
+    expect(fizzBuzz(number)).toEqual(["fizz"]);
   });
 
   test('Should return fizz if 6 given param', () => {
-    const number = 6;
-    expect(fizzBuzz(number)).toBe('fizz');
+    const number = [6];
+    expect(fizzBuzz(number)).toEqual(["fizz"]);
   });
 
   test('Should return buzz if 5 given param', () => {
-    const number = 5;
-    expect(fizzBuzz(5)).toBe('buzz');
+    const number = [5];
+    expect(fizzBuzz(number)).toEqual(["buzz"]);
+  });
+
+  test('Should return buzz if 10 given param', () => {
+    const number = [10];
+    expect(fizzBuzz(number)).toEqual(["buzz"]);
+  });
+
+  test('Should return buzz if 15 given param', () => {
+    const number = [15];
+    expect(fizzBuzz(number)).toEqual(['fizzbuzz']);
+  });
+
+  test('Should return the correct value if array given', () => {
+    const array = [3, 5];
+    expect(fizzBuzz(array)).toEqual(['fizz', 'buzz']);
+  });
+  
+  test('Should return the correct value if big array given', () => {
+    const array = [1, 2, 3, 5, 10, 11, 15, 13];
+    expect(fizzBuzz(array)).toEqual([1, 2, "fizz", "buzz", "buzz", 11, "fizzbuzz", 13]);
   });
 })
